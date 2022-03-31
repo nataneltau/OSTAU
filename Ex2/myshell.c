27819c1a161e6eef22d3c3c2d30b605593f05b29
natanel <natanel@mail.tau.ac.il>
2022-03-31 18:59:36 +0300

    update 1.2 - added signal handlers

--- Ex2/myshell.c
+++ Ex2/myshell.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
 
 /* Credits:
  * https://www.youtube.com/watch?v=PIb2aShU_H4 - help understand dup2() function
+ * https://www.youtube.com/watch?v=Z2THA6jX3j8 - help with SIGCHLD
 */
 
 
@@ -17,13 +23,15 @@
 */
 int tagCommands(int count, char **arglist);
 
-void mySignalHandler(int signum){
+void freeChild(int signum){
+
+
 
 }//end of function mySignalHandler
 
 int prepare(void){
 
-    //make you shall not pass!  ?
+    signal(SIGINT, SIG_IGN);//you shall not pass!
     return 0;
 }//end of function prepare
 
@@ -36,13 +44,15 @@ int process_arglist(int count, char **arglist){
 
     int tag = tagCommands(count, arglist); 
     int pid ,status;
-
-
+    signal(SIGINT, SIG_IGN);//you shall not pass! also work for background processes
+    signa(SIGCHLD, SIG_IGN);//prevent zombies 
 
     if(tag == 1){//we are in case: executing commands , section 1.2.1
-        //pid = fork();
+        pid = fork();
 
         if(pid == 0){// we are a child
+            signal(SIGINT, SIG_DFL);
+            signal(SIGCHLD, SIG_DFL);
             execvp(*arglist[0], *arglist);
         }
         else{//we are parents
@@ -52,16 +62,17 @@ int process_arglist(int count, char **arglist){
 
     }//end of if(tag == 1)
 
-    else if(tag == 2 && pid == 0){ //we are in case: executing commands in the background , section 1.2.2 && the parent shouldn't get inside this block
+    else if(tag == 2){ //we are in case: executing commands in the background , section 1.2.2 
         
-        *arglist[count-1] = NULL;//remove & from arglist
+        *arglist[count-1] = NULL;//remove "&" from arglist
         
-        //pid = fork();
+        pid = fork();
 
-        // we are a child
-        execvp(*arglist[0], *arglist);
-        
-
+        if(pid == 0){// we are a child
+            signal(SIGCHLD, SIG_DFL);
+            execvp(*arglist[0], *arglist);
+        }
+    
 
     }//end of if(tag == 2)
 
@@ -74,7 +85,7 @@ int process_arglist(int count, char **arglist){
 
         countOfPipe--;//because the ++ in the while loop we need to go one back
 
-        arglist[countOfPipe] = NULL;//before this, was arglist[countOfPipe] == '|'
+        arglist[countOfPipe] = NULL;//before this, was arglist[countOfPipe] == "|"
 
         //to one child send arglist as it is to other send arglist+countOfPipe+1
 
@@ -85,6 +96,8 @@ int process_arglist(int count, char **arglist){
         pid = fork();
 
         if(pid == 0){//first child
+            signal(SIGINT, SIG_DFL);
+            signal(SIGCHLD, SIG_DFL);
             close(pfds[0]);
             dup2(pfds[1], STDOUT_FILENO);
             close(pfds[1]);
@@ -96,7 +109,9 @@ int process_arglist(int count, char **arglist){
             //add?:                 waitpid(pid, &status, 0);
             anotherPid = fork();
 
-            if(anotherPid == 0){
+            if(anotherPid == 0){//second child
+                signal(SIGINT, SIG_DFL);
+                signal(SIGCHLD, SIG_DFL);
                 close(pfds[1]);
                 dup2(pfds[0], STDIN_FILENO);
                 close(pfds[0]);
@@ -114,22 +129,21 @@ int process_arglist(int count, char **arglist){
 
     else{ // we are in case: output redirecting , section 1.2.4
         
-        int redirectOut = open(*arglist[count-1], O_CREAT | O_APPEND);//open the file
+        *arglist[count-2] = NULL;//remove ">>" from arglist
 
         pid = fork();
 
         if(pid == 0){// we are a child
+            int redirectOut = open(*arglist[count-1], O_CREAT | O_APPEND);//open the file
+            signal(SIGINT, SIG_DFL);
+            signal(SIGCHLD, SIG_DFL);
             dup2(redirectOut, STDOUT_FILENO);//redirect so the output will be written to the file
             close(redirectOut);//close the file
             execvp(*arglist[0], *arglist);
         }
         else{//we are parents
             waitpid(pid, &status, 0);
-            close(redirectOut);//close the file
         }
-
-        
-
 
     }//end of else which is equal to if(tag==4)
 
